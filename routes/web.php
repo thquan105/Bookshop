7e3a65ec3dd399a86b4cc594a99ea00e30e047b6
= <tuongdang297@gmail.com>
2023-11-15 10:03:57 +0700

    CRUD categories

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group([ 'prefix' => 'admin', 'as' => 'admin.'], function() {
+        Route::resource('categories', CategoryController::class);
 });
