f1856eefe2b11c6f998dcef216bbf7af049723b4
Thquan105 <thquan105@gmail.com>
2023-11-17 11:44:20 +0700

    Add slug

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,15 +6,31 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 
 class Product extends Model implements HasMedia
 {
-    use HasFactory, InteractsWithMedia;
+    use HasFactory, InteractsWithMedia, Sluggable;
 
     protected $guarded = ['id','created_at', 'updated_at'];
 
     protected $appends = ['gallery'];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name',
+                'onUpdate' => true
+            ]
+        ];
+    }
 
 
     public function getGalleryAttribute()
--- database/migrations/2023_11_13_140916_create_products_table.php
+++ database/migrations/2023_11_13_140916_create_products_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->integer('price');
+            $table->string('slug');
             $table->integer('quantity');
             $table->text('description');
             $table->text('details');
