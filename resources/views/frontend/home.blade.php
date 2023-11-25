0bf4fa7b18225dbc95fad8895d8f9591ffea8439
Thquan105 <thquan105@gmail.com>
2023-11-25 23:27:36 +0700

    Banner in Home

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -48,18 +48,7 @@
                     <div class="block1 wrap-pic-w">
                         <img src="{{ asset('frontend/images/banner-04.jpg') }}" alt="IMG-BANNER">
 
-                        <a href="product.html"
-                            class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
-                            <div class="block1-txt-child1 flex-col-l">
-                                <span class="block1-name ltext-102 trans-04 p-b-8">
-                                    Women
-                                </span>
-
-                                <span class="block1-info stext-102 trans-04">
-                                    New Trend
-                                </span>
-                            </div>
-
+                        <a class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
                             <div class="block1-txt-child2 p-b-4 trans-05">
                                 <div class="block1-link stext-101 cl0 trans-09">
                                     Shop Now
@@ -74,18 +63,7 @@
                     <div class="block1 wrap-pic-w">
                         <img src="{{ asset('frontend/images/banner-05.jpg') }}" alt="IMG-BANNER">
 
-                        <a href="product.html"
-                            class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
-                            <div class="block1-txt-child1 flex-col-l">
-                                <span class="block1-name ltext-102 trans-04 p-b-8">
-                                    Men
-                                </span>
-
-                                <span class="block1-info stext-102 trans-04">
-                                    New Trend
-                                </span>
-                            </div>
-
+                        <a class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
                             <div class="block1-txt-child2 p-b-4 trans-05">
                                 <div class="block1-link stext-101 cl0 trans-09">
                                     Shop Now
@@ -98,20 +76,9 @@
                 <div class="col-md-6 col-lg-4 p-b-30 m-lr-auto">
                     <!-- Block1 -->
                     <div class="block1 wrap-pic-w">
-                        <img src="{{ asset('frontend/images/banner-07.jpg') }}" alt="IMG-BANNER">
+                        <img src="{{ asset('frontend/images/banner-07.png') }}" alt="IMG-BANNER">
 
-                        <a href="product.html"
-                            class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
-                            <div class="block1-txt-child1 flex-col-l">
-                                <span class="block1-name ltext-102 trans-04 p-b-8">
-                                    Watches
-                                </span>
-
-                                <span class="block1-info stext-102 trans-04">
-                                    Spring 2018
-                                </span>
-                            </div>
-
+                        <a class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
                             <div class="block1-txt-child2 p-b-4 trans-05">
                                 <div class="block1-link stext-101 cl0 trans-09">
                                     Shop Now
@@ -124,20 +91,9 @@
                 <div class="col-md-6 col-lg-4 p-b-30 m-lr-auto">
                     <!-- Block1 -->
                     <div class="block1 wrap-pic-w">
-                        <img src="{{ asset('frontend/images/banner-08.jpg') }}" alt="IMG-BANNER">
+                        <img src="{{ asset('frontend/images/banner-08.png') }}" alt="IMG-BANNER">
 
-                        <a href="product.html"
-                            class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
-                            <div class="block1-txt-child1 flex-col-l">
-                                <span class="block1-name ltext-102 trans-04 p-b-8">
-                                    Bags
-                                </span>
-
-                                <span class="block1-info stext-102 trans-04">
-                                    Spring 2018
-                                </span>
-                            </div>
-
+                        <a class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
                             <div class="block1-txt-child2 p-b-4 trans-05">
                                 <div class="block1-link stext-101 cl0 trans-09">
                                     Shop Now
@@ -150,20 +106,10 @@
                 <div class="col-md-6 col-lg-4 p-b-30 m-lr-auto">
                     <!-- Block1 -->
                     <div class="block1 wrap-pic-w">
-                        <img src="{{ asset('frontend/images/banner-09.jpg') }}" alt="IMG-BANNER">
+                        <img src="{{ asset('frontend/images/banner-09.png') }}" alt="IMG-BANNER">
 
-                        <a href="product.html"
+                        <a
                             class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
-                            <div class="block1-txt-child1 flex-col-l">
-                                <span class="block1-name ltext-102 trans-04 p-b-8">
-                                    Accessories
-                                </span>
-
-                                <span class="block1-info stext-102 trans-04">
-                                    Spring 2018
-                                </span>
-                            </div>
-
                             <div class="block1-txt-child2 p-b-4 trans-05">
                                 <div class="block1-link stext-101 cl0 trans-09">
                                     Shop Now
